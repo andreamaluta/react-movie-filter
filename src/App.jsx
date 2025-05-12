956b4beb9fa2b77183df31a25808416fe0f5dd15
andreamaluta <andreamaluta@gmail.com>
2025-05-12 14:34:01 +0200

    [UPD] copied the array and create the list of film

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const initialFilm = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+  const [films, setFilms] = useState(initialFilm);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1 className='mb-4'>Film Filter</h1>
+        <ul className="list-group">
+          {films.map((film, index) => (
+            <li key={index} className='list-group-item'>
+              {film.title}
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
